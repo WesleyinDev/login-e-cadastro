fdfe16a2af22c1598380f680e2a84b6c52944515
Wesley <devwesley@hotmail.com>
2023-12-30 13:22:09 -0300

    Versão: 2.0.0  Estrutura para barra a criação de campos vazios implemetada.    Arquivos modificados: 	modified:   assets/css/style.css 	modified:   assets/js/cadastro.js 	modified:   assets/js/login.js 	modified:   cadastro.html

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -8,18 +8,27 @@ function cadastro (event) {
 
     const cadastroUsuario = document.querySelector('#cadastro-usuario').value;
     const cadastroSenha = document.querySelector('#cadastro-senha').value;
+    const cadastroNome = document.querySelector('#cadastro-nome').value;
+    const cadastroEmail = document.querySelector('#cadastro-email').value;
     const sucessCadastro = document.querySelector('.cadastro-sucess');
 
+    if (cadastroUsuario === '' || cadastroSenha === '' || cadastroNome === '' || cadastroEmail === '' ) {
+        sucessCadastro.classList.add('erro-login');
+        sucessCadastro.innerHTML = `Preencha todos os campos por favor !`
+        return ;
+    }
 
+
+    sucessCadastro.classList.remove('erro-login')
     sucessCadastro.classList.add('sucesso-cadastro')
-    sucessCadastro.innerHTML = 'Cadastro realizado com sucesso';
+    sucessCadastro.innerHTML = 'Cadastro realizado com sucesso, redirecionando você !!';
 
     localStorage.setItem('cadastroUsuario', cadastroUsuario);
     localStorage.setItem('cadastroSenha', cadastroSenha);
 
     setTimeout (function () {
         window.location.href ='index.html';
-    }, 3000);
+    }, 2000);
 
 
 }
--- assets/js/login.js
+++ assets/js/login.js
@@ -3,15 +3,20 @@ const loginSucess = document.querySelector('.login-sucess');
 
 function login(event) {
     event.preventDefault();
-
-
     
     const loginUsuario = localStorage.getItem('cadastroUsuario');
     const loginSenha = localStorage.getItem('cadastroSenha');
     const usuario = document.querySelector('#usuario').value;
     const senha = document.querySelector('#senha').value;
 
+    if (usuario === '' || senha === '') {
+        loginSucess.classList.add('erro-login');
+        loginSucess.innerHTML = `Preencha todos os campos por favor !`;
+        return ;
+    }
+
     if (usuario === loginUsuario && senha === loginSenha) {
+        loginSucess.classList.remove('erro-login');
         loginSucess.classList.add('sucesso-login');
         loginSucess.innerHTML = 'Seja bem-vindo. Entrando ...';
     }else {
